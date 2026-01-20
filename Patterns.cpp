61a9a6fd6e3299e86c07ef1407c622db0f16576e
nehxxa <gaikwadneha206@gmail.com>
2026-01-20 17:59:38 +0530

    Test commit

--- Patterns.cpp
+++ Patterns.cpp
@@ -155,18 +155,18 @@ using namespace std;
 
 // ===============================================
 
-int main(){
+// int main(){
 
-    int n=4;
+//     int n=4;
 
-    for (int i=0;i<n;i++){
+//     for (int i=0;i<n;i++){
         
-        for(int j=0;j<i;j++){
-            cout<<" ";    
-        }
-        for(int j=0;j<=n-i;j++){
-            cout<<(i+1);
-        }
-        cout<<endl;
-    }
-}
+//         for(int j=0;j<i;j++){
+//             cout<<" ";    
+//         }
+//         for(int j=0;j<=n-i;j++){
+//             cout<<(i+1);
+//         }
+//         cout<<endl;
+//     }
+// }
