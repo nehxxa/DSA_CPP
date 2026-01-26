ef4e6c05df68be1b9cbe606d554db3a4a679f210
nehxxa <gaikwadneha206@gmail.com>
2026-01-26 15:48:24 +0530

    26-1-2026

--- Patterns.cpp
+++ Patterns.cpp
@@ -170,3 +170,26 @@ using namespace std;
 //         cout<<endl;
 //     }
 // }
+
+int main(){
+    int n=4;
+
+    for(int i=1;i<n;i++){
+
+
+        for(int j=0;j<n-i;j++){
+            cout<<" ";
+    }
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+
+        for(int j=i-1;j>=1;j--){
+            cout<<j;
+
+        }
+
+        cout<<endl; 
+    }
+    
+}
